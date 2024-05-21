d85e71712f056ef6f5a66ea187f3e20e989323e1
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-05-21 17:11:13 +0200

    v2

--- script/show.js
+++ script/show.js
@@ -10,3 +10,19 @@ function showNav(){
     })
   }
   showNav();
+
+/* Для всплывающего окна */ 
+const menuBtn = document.querySelector('.menuBtn');
+const menu = document.querySelector('nav');
+menuBtn.addEventListener('click', () =>{
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('popUp');
+});
+document.addEventListener('click', function(event) {
+    // Проверяем, что клик произошел вне меню и кнопки открытия
+    if (!menu.contains(event.target) && !menuBtn.contains(event.target)) {
+        // Закрываем меню, добавляя класс, который изменяет его видимость или другие свойства
+        menu.classList.remove('popUp');
+        menuBtn.classList.remove('active');
+    }
+});
